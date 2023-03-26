0dde489c797ac285c342eb440899c69fe007e17e
Wayne Jessen <wjessen@thefarmersdog.com>
2023-03-25 22:42:50 -0700

    react app reading and parsing from the docs app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Papa from 'papaparse';
 
 function App() {
+
+  const [data, setData] = useState({});
+  const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ0ZopKDj4iCxxsYv1Y-JKSUSRCgpBlOXHOMHwoFuUx21fjOSWYGKEXZSDEz3tqptVKzCbU0zphwG0E/pub?output=csv"
+  Papa.parse(url, {
+    download: true,
+    complete: function(results) {
+      setData(results);
+      console.log(data);
+    }
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <table>
+  <thead>
+  
+    <tr>
+      {data.data[0].map((x) => <th>{x}</th>)}
+    </tr>
+  </thead>
+  <tbody>
+    {data.data.slice(1).map((x) => <tr>{
+      x.map((y) => <td>{y}</td>)
+    }</tr>)} 
+    </tbody>
+</table>
     </div>
   );
 }
