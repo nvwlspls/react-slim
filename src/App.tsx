8e040b5460efee908e513e48fba4f1b3bc07ea89
Wayne Jessen <wjessen@thefarmersdog.com>
2023-03-27 21:29:16 -0700

    before removing papaparse

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,33 @@
 import { useState } from 'react';
-import Papa from 'papaparse';
+import * as papaparse from 'papaparse';
 
-function App() {
+type AppProps = {
+
+};
+
+function App(): JSX.Element {
 
   const [data, setData] = useState({});
   const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ0ZopKDj4iCxxsYv1Y-JKSUSRCgpBlOXHOMHwoFuUx21fjOSWYGKEXZSDEz3tqptVKzCbU0zphwG0E/pub?output=csv"
-  Papa.parse(url, {
+
+  papaparse.parse(url, {
     download: true,
     complete: function(results) {
       setData(results);
-      console.log(data);
+      console.log("now")
     }
-  })
+  });
 
   return (
     <div className="App">
     <table>
   <thead>
-  
     <tr>
-      {data.data[0].map((x) => <th>{x}</th>)}
+
     </tr>
-  </thead>
+    </thead>
+
   <tbody>
-    {data.data.slice(1).map((x) => <tr>{
-      x.map((y) => <td>{y}</td>)
-    }</tr>)} 
     </tbody>
 </table>
     </div>
